c2bff1fd5da28633925cdded5fc5387dc1e863a3
yajiedesign@gmail.com <yajiedesign@gmail.com>
2015-02-26 22:40:14 +0800

    First Commt

---
+++ ShipShieldPlugin/ShipShield.cs
@@ -0,0 +1,169 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipShieldPlugin
+{
+    class ShipShieldE
+    {
+        //   public BeaconEntity CenterBeaconEntity { get; set; }
+        public IMyFaction Faction { get; set; }
+
+        //public Vector3D CenterBeaconPosition { get; set; }
+
+        public IMyEntity CenterBeaconIMyEntity { get; set; }
+    }
+
+
+
+    public class ShipShield 
+    {
+        public static readonly string ShipShieldSubtypeId = "ShipShield"; 
+        public ShipShield()
+        {
+           
+        }
+
+
+
+        public Guid Id
+        {
+            get
+            {
+                GuidAttribute guidAttr = (GuidAttribute)typeof(ShipShield).Assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+                return new Guid(guidAttr.Value);
+            }
+        }
+
+
+
+        public void Init()
+        {
+            try
+            {
+
+
+
+            File.Copy("Sandbox.Game.dll", "Sandbox.Game.dll.tmp",true);
+                var sandboxasm = AssemblyDefinition.ReadAssembly("Sandbox.Game.dll.tmp");
+                if (sandboxasm != null)
+                {
+                    var MyProjectileDef = sandboxasm.Modules
+                        .SelectMany(m => m.Types)
+                        .Where(t => t.Name == "MyProjectile").First();
+
+                    var DoDamageDef = MyProjectileDef.Methods.FirstOrDefault(f => f.Name == "DoDamage");
+                    var work = DoDamageDef.Body.GetILProcessor();
+
+                    if (DoDamageDef.Body.Instructions.First().OpCode != OpCodes.Call)
+                        
+                    {
+                        return;
+                    }
+                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Call,
+                     sandboxasm.MainModule.Import(typeof(ShipShield).GetMethod("MyProjectileDoDamage"))));
+                    work.InsertAfter(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Brfalse_S, DoDamageDef.Body.Instructions[3]));
+
+
+                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[1]));
+                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[0]));
+
+
+                    MemoryStream sandboxStream = new MemoryStream();
+
+                    File.Copy("Sandbox.Game.dll", "Sandbox.Game.dll.bak", true);
+                    sandboxasm.Write("Sandbox.Game.dll");
+
+
+                    //var changeSandboxAssembly = Assembly.Load(sandboxStream.ToArray());
+
+                    //var changeMyProjectile = changeSandboxAssembly.GetType("Sandbox.Game.Weapons.MyProjectile");
+
+                    //MethodInfo changeMyProjectileDoDamage = changeMyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
+                    //var ilCodes = changeMyProjectileDoDamage.GetMethodBody().GetILAsByteArray();
+
+
+                    //MethodInfo MyProjectileDoDamage = MyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
+                    //InjectionHelper.UpdateILCodes(MyProjectileDoDamage, ilCodes);
+                }
+            }
+            catch (Exception e)
+            {
+                //Logging.WriteLineAndConsole(e.ToString());
+            }
+
+        }
+
+
+        public static bool MyProjectileDoDamage(ref VRageMath.Vector3D hitPosition, ref IMyEntity damagedEntity)
+        {
+            var cubeGrid = damagedEntity as IMyCubeGrid;
+            if (cubeGrid==null)
+            {
+                return true;
+            }
+            try
+            {
+                List<IMySlimBlock> shieldblocks = new List<IMySlimBlock>();
+                cubeGrid.GetBlocks(shieldblocks, (block) => {
+                    if (block.FatBlock==null)
+                    {
+                        return false;
+                    }
+                    return block.FatBlock.BlockDefinition.SubtypeId == ShipShieldSubtypeId;
+                });
+
+                foreach (var shielditem in shieldblocks)
+                {
+                    if (shielditem.FatBlock.IsFunctional && shielditem.FatBlock.IsWorking)
+                    {
+
+                        hitPosition = shielditem.FatBlock.WorldMatrix.Translation;
+                        break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+               // Logging.WriteLineAndConsole(e.ToString());
+            }
+  
+
+
+            //Logging.WriteLineAndConsole("DoDamage");
+            return true;
+        }
+
+        public void Shutdown()
+        {
+           // Logging.WriteLineAndConsole("Shutdown");
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return "Dedicated Server ShipShield"; }
+        }
+
+        public string Version
+        {
+            get { return typeof(ShipShield).Assembly.GetName().Version.ToString(); }
+        }
+
+
+
+    }
+}
---
+++ ShipShieldTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using ShipShieldPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipShieldPatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShipShield shipShield = new ShipShield();
+            shipShield.Init();
+        }
+    }
+}
