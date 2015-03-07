576ca2f7df10bf23ce02bb90c35ba1ebcb80024a
yajiedesign@gmail.com <yajiedesign@gmail.com>
2015-03-07 09:17:52 +0800

    use patch

--- ShipShieldPlugin/ShipShield.cs
+++ ShipShieldPlugin/ShipShield.cs
@@ -1,7 +1,6 @@
 ﻿using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Sandbox.ModAPI;
-using ShipShieldPlugin.Utility;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,24 +13,22 @@ using System.Threading.Tasks;
 
 namespace ShipShieldPlugin
 {
-    class ShipShieldE
+  
+
+    public class ShipShield
     {
-        //   public BeaconEntity CenterBeaconEntity { get; set; }
-        public IMyFaction Faction { get; set; }
+        public static string CubeBlockNamespace = "6DDCED906C852CFDABA0B56B84D0BD74";
+        public static string CubeBlockClass = "54A8BE425EAC4A11BFF922CFB5FF89D0";
 
-        //public Vector3D CenterBeaconPosition { get; set; }
-
-        public IMyEntity CenterBeaconIMyEntity { get; set; }
-    }
-
+        //public static string CubeBlockDamageBlockMethod = "165EAAEA972A8C5D69F391D030C48869";
+        //public static string CubeBlockDamageBlockMethod = "DoDamage";
+        public static string CubeBlockDamageBlockMethod = "165EAAEA972A8C5D69F391D030C48869";
 
 
-    public class ShipShield : SEModAPIExtensions.API.Plugin.IPlugin
-    {
-        public static readonly string ShipShieldSubtypeId = "ShipShield"; 
+        public static readonly string ShipShieldSubtypeId = "ShipShield";
         public ShipShield()
         {
-           
+
         }
 
 
@@ -52,89 +49,104 @@ namespace ShipShieldPlugin
             try
             {
 
-                InjectionHelper.Initialize();
-                InjectionHelper.WaitForIntializationCompletion();
 
-                var MyProjectile = SEModAPIInternal.API.Common.SandboxGameAssemblyWrapper.Instance.GetAssemblyType("Sandbox.Game.Weapons", "MyProjectile");
 
-     
-
-                var sandboxasm = AssemblyDefinition.ReadAssembly("Sandbox.Game.dll");
+                File.Copy("Sandbox.Game.dll", "Sandbox.Game.dll.tmp", true);
+                var sandboxasm = AssemblyDefinition.ReadAssembly("Sandbox.Game.dll.tmp");
                 if (sandboxasm != null)
                 {
-                    var MyProjectileDef = sandboxasm.Modules
-                        .SelectMany(m => m.Types)
-                        .Where(t => t.Name == "MyProjectile").First();
+                    bool iswrite = false;
 
-                    var DoDamageDef = MyProjectileDef.Methods.FirstOrDefault(f => f.Name == "DoDamage");
-                    var work = DoDamageDef.Body.GetILProcessor();
+                    iswrite |= MyProjectileChange(sandboxasm);
 
-                    if (DoDamageDef.Body.Instructions.First().OpCode != OpCodes.Call)
-                        
+
+                    iswrite |= MyMissileChange(sandboxasm);
+
+
+
+                    if (iswrite)
                     {
-                        return;
+                        MemoryStream sandboxStream = new MemoryStream();
+                        File.Copy("Sandbox.Game.dll", "Sandbox.Game.dll.bak", true);
+                        sandboxasm.Write("Sandbox.Game.dll");
                     }
-                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Call,
-                     sandboxasm.MainModule.Import(typeof(ShipShield).GetMethod("MyProjectileDoDamage"))));
-                    work.InsertAfter(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Brfalse_S, DoDamageDef.Body.Instructions[3]));
+
+                    //var changeSandboxAssembly = Assembly.Load(sandboxStream.ToArray());
+
+                    //var changeMyProjectile = changeSandboxAssembly.GetType("Sandbox.Game.Weapons.MyProjectile");
+
+                    //MethodInfo changeMyProjectileDoDamage = changeMyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
+                    //var ilCodes = changeMyProjectileDoDamage.GetMethodBody().GetILAsByteArray();
 
 
-                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[1]));
-                    work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[0]));
-
-
-                    MemoryStream sandboxStream = new MemoryStream();
-
-                    sandboxasm.Write(sandboxStream);
-
-
-                    var changeSandboxAssembly = Assembly.Load(sandboxStream.ToArray());
-
-                    var changeMyProjectile = changeSandboxAssembly.GetType("Sandbox.Game.Weapons.MyProjectile");
-
-                    MethodInfo changeMyProjectileDoDamage = changeMyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
-                    var ilCodes = changeMyProjectileDoDamage.GetMethodBody().GetILAsByteArray();
-
-                    FileStream fs = new FileStream("outil.bin", FileMode.Create);
-                    fs.Write(ilCodes, 0, ilCodes.Length);
-                    fs.Close();
-
-
-                    MethodInfo replaceMethod = this.GetType().GetMethod("MyProjectileDoDamage", BindingFlags.Public | BindingFlags.Static);
-                    Logging.WriteLineAndConsole("replaceMethod.MetadataToken"+ replaceMethod.MetadataToken);
-
-
-                    MethodInfo MyProjectileDoDamage = changeMyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
-                    InjectionHelper.UpdateILCodes(MyProjectileDoDamage, ilCodes);
-
-
-                    var mpi = changeSandboxAssembly.CreateInstance("Sandbox.Game.Weapons.MyProjectile");
-
-
-
-                    MyProjectileDoDamage.Invoke(mpi, new object[] { new VRageMath.Vector3D(), null });
+                    //MethodInfo MyProjectileDoDamage = MyProjectile.GetMethod("DoDamage", BindingFlags.NonPublic | BindingFlags.Instance);
+                    //InjectionHelper.UpdateILCodes(MyProjectileDoDamage, ilCodes);
                 }
             }
             catch (Exception e)
             {
-                Logging.WriteLineAndConsole(e.ToString());
+                //Logging.WriteLineAndConsole(e.ToString());
             }
 
+        }
+
+        private static bool MyMissileChange(AssemblyDefinition sandboxasm)
+        {
+            //var MyMissileDef = sandboxasm.Modules
+            //.SelectMany(m => m.Types)
+            //.Where(t => t.Name == "MyProjectile").First();
+
+            //var UpdateBeforeSimulationDef = MyMissileDef.Methods.FirstOrDefault(f => f.Name == "DoDamage");
+            //var work = UpdateBeforeSimulationDef.Body.GetILProcessor();
+
+            //if (UpdateBeforeSimulationDef.Body.Instructions.First().OpCode == OpCodes.Call)
+            //{
+            //    work.InsertBefore(UpdateBeforeSimulationDef.Body.Instructions.First(), work.Create(OpCodes.Call,
+            //        sandboxasm.MainModule.Import(typeof(ShipShield).GetMethod("MyProjectileDoDamage"))));
+            //    work.InsertAfter(UpdateBeforeSimulationDef.Body.Instructions.First(), work.Create(OpCodes.Brfalse_S, UpdateBeforeSimulationDef.Body.Instructions[3]));
+            //    work.InsertBefore(UpdateBeforeSimulationDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, UpdateBeforeSimulationDef.Parameters[1]));
+            //    work.InsertBefore(UpdateBeforeSimulationDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, UpdateBeforeSimulationDef.Parameters[0]));
+            //    return true;
+            //}
+            return false;
+            
+        }
+
+        private static bool MyProjectileChange(AssemblyDefinition sandboxasm)
+        {
+            var MyProjectileDef = sandboxasm.Modules
+                .SelectMany(m => m.Types)
+                .Where(t => t.Name == "MyProjectile").First();
+
+            var DoDamageDef = MyProjectileDef.Methods.FirstOrDefault(f => f.Name == "DoDamage");
+            var work = DoDamageDef.Body.GetILProcessor();
+
+            if (DoDamageDef.Body.Instructions.First().OpCode == OpCodes.Call)
+            {
+                work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Call,
+                    sandboxasm.MainModule.Import(typeof(ShipShield).GetMethod("MyProjectileDoDamage"))));
+                work.InsertAfter(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Brfalse_S, DoDamageDef.Body.Instructions[3]));
+                work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[1]));
+                work.InsertBefore(DoDamageDef.Body.Instructions.First(), work.Create(OpCodes.Ldarga_S, DoDamageDef.Parameters[0]));
+                return true;
+            }
+            return false;
         }
 
 
         public static bool MyProjectileDoDamage(ref VRageMath.Vector3D hitPosition, ref IMyEntity damagedEntity)
         {
             var cubeGrid = damagedEntity as IMyCubeGrid;
-            if (cubeGrid==null)
+            if (cubeGrid == null)
             {
                 return true;
             }
             try
             {
                 List<IMySlimBlock> shieldblocks = new List<IMySlimBlock>();
-                cubeGrid.GetBlocks(shieldblocks, (block) => {
-                    if (block.FatBlock==null)
+                cubeGrid.GetBlocks(shieldblocks, (block) =>
+                {
+                    if (block.FatBlock == null)
                     {
                         return false;
                     }
@@ -145,17 +157,16 @@ namespace ShipShieldPlugin
                 {
                     if (shielditem.FatBlock.IsFunctional && shielditem.FatBlock.IsWorking)
                     {
-
                         hitPosition = shielditem.FatBlock.WorldMatrix.Translation;
                         break;
                     }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-               // Logging.WriteLineAndConsole(e.ToString());
+                // Logging.WriteLineAndConsole(e.ToString());
             }
-  
+
 
 
             //Logging.WriteLineAndConsole("DoDamage");
@@ -164,7 +175,7 @@ namespace ShipShieldPlugin
 
         public void Shutdown()
         {
-           // Logging.WriteLineAndConsole("Shutdown");
+            // Logging.WriteLineAndConsole("Shutdown");
         }
 
         public void Update()
